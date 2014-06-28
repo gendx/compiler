c92e0cb83e022c3b5f6ccb4cbf1e38ecdefefb7f
G. Endignoux <ggendx@gmail.com>
2014-06-28 17:07:35 +0200

    Modified install script

--- install-bison-flex-c++.sh
+++ install-bison-flex-c++.sh
@@ -4,18 +4,26 @@ git clone http://git.code.sf.net/p/bobcat/code bobcat/
 git clone http://git.code.sf.net/p/flexcpp/code flexcpp/
 git clone http://git.code.sf.net/p/bisoncpp/code bisoncpp/
 
-icmake/icmake/icm_bootstrap /
-icmake/icmake/install strip progs
+cd icmake/icmake
+./icm_bootstrap /
+./install strip progs
+cd ../..
 
-echo 'n\nn\n' | bobcat/bobcat/build libraries strip
-bobcat/bobcat/build install
+cd bobcat/bobcat
+echo 'n\nn\n' | ./build libraries strip
+./build install
+cd ../..
 
-flexcpp/flexc++/build program strip
-flexcpp/flexc++/build install program
-flexcpp/flexc++/build install skel
+cd flexcpp/flexc++
+./build program strip
+./build install program
+./build install skel
+cd ../..
 
-bisoncpp/bisonc++/build program strip
-bisoncpp/bisonc++/build install
+cd bisoncpp/bisonc++
+./build program strip
+./build install
+cd ../..
 
 #rm -Rf icmake bobcat flexcpp bisoncpp
 
