82a59c260a5112cc23e8fec3ce78534414ce5e26
G. Endignoux <ggendx@gmail.com>
2014-06-28 18:50:19 +0200

    Modified install script

--- install-bison-flex-c++.sh
+++ install-bison-flex-c++.sh
@@ -13,7 +13,7 @@ echo "\n***** icmake bootstrap done *****\n"
 echo "\n***** icmake install done *****\n"
 cd ../..
 
-#cp -f bisoncpp/bisonc++/CLASSES.bobcat bobcat/bobcat/CLASSES
+cp -f bisoncpp/bisonc++/CLASSES.bobcat bobcat/bobcat/CLASSES
 cd bobcat/bobcat
 echo "\n\n\n\n" | ./build libraries strip
 echo "\n***** bobcat build done *****\n"
