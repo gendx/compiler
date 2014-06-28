09980ccb5c6996e640f9c28b458cf6db97004626
G. Endignoux <ggendx@gmail.com>
2014-06-28 18:24:12 +0200

    Modified install script

--- install-bison-flex-c++.sh
+++ install-bison-flex-c++.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 git clone http://git.code.sf.net/p/icmake/code icmake/
-#git clone http://git.code.sf.net/p/bobcat/code bobcat/
+git clone http://git.code.sf.net/p/bobcat/code bobcat/
 git clone http://git.code.sf.net/p/flexcpp/code flexcpp/
 git clone http://git.code.sf.net/p/bisoncpp/code bisoncpp/
 
-apt-get install libbobcat-dev
+#apt-get install libbobcat-dev
 
 cd icmake/icmake
 ./icm_bootstrap /
 echo "\n***** icmake bootstrap ok *****\n"
-./icm_install strip progs
+./icm_install strip all
 echo "\n***** icmake install ok *****\n"
 cd ../..
 
-#cp -f bisoncpp/bisonc++/CLASSES.bobcat bobcat/bobcat/CLASSES
-#cd bobcat/bobcat
-#echo "\n\n\n\n" | ./build libraries strip
-#echo "\n***** bobcat build ok *****\n"
-#./build install
-#echo "\n***** bobcat install ok *****\n"
-#cd ../..
+cp -f bisoncpp/bisonc++/CLASSES.bobcat bobcat/bobcat/CLASSES
+cd bobcat/bobcat
+echo "\n\n\n\n" | ./build libraries strip
+echo "\n***** bobcat build ok *****\n"
+./build install
+echo "\n***** bobcat install ok *****\n"
+cd ../..
 
 cd flexcpp/flexc++
 ./build program strip
