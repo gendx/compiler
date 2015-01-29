f92b13175a2ea09545014b0775f37c8f47d83a1a
G. Endignoux <ggendx@gmail.com>
2015-01-29 23:44:29 +0100

    Fixed Clang installation

--- install/install-clang.sh
+++ install/install-clang.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
-apt-add-repository 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise main'
+apt-add-repository 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.5 main'
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | apt-key add -
 apt-get update -qq
 update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.5 90
